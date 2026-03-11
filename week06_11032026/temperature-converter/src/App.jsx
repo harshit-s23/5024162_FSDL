405a25d53a40779dafae85293fd85c828e82c32e
harshit-s23 <harshitamit23@gmail.com>
2026-03-11 07:01:45 +0000

    React 2

---
+++ week06_11032026/temperature-converter/src/App.jsx
@@ -0,0 +1,13 @@
+import Converter from "./components/Converter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Temperature Converter</h1>
+      <Converter />
+    </div>
+  );
+}
+
+export default App;
---
+++ week06_11032026/temperature-converter/src/components/Converter.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function Converter() {
+  const [celsius, setCelsius] = useState("");
+  const [fahrenheit, setFahrenheit] = useState("");
+
+  const convertToFahrenheit = () => {
+    if (celsius === "") return;
+    const f = (celsius * 9) / 5 + 32;
+    setFahrenheit(f.toFixed(2));
+  };
+
+  const convertToCelsius = () => {
+    if (fahrenheit === "") return;
+    const c = ((fahrenheit - 32) * 5) / 9;
+    setCelsius(c.toFixed(2));
+  };
+
+  return (
+    <div className="converter">
+
+      <div className="box">
+        <label>Celsius</label>
+        <input
+          type="number"
+          value={celsius}
+          onChange={(e) => setCelsius(e.target.value)}
+        />
+        <button onClick={convertToFahrenheit}>
+          Convert to Fahrenheit
+        </button>
+      </div>
+
+      <div className="box">
+        <label>Fahrenheit</label>
+        <input
+          type="number"
+          value={fahrenheit}
+          onChange={(e) => setFahrenheit(e.target.value)}
+        />
+        <button onClick={convertToCelsius}>
+          Convert to Celsius
+        </button>
+      </div>
+
+    </div>
+  );
+}
+
+export default Converter;
